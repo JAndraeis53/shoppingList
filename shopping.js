40fe26c8b65f74601f496dcd1904d8689d9dde53
Jalon Duncan <duncanjalon@gmail.com>
2021-06-01 11:28:53 -0500

    inital commit

---
+++ shopping.js
@@ -0,0 +1,42 @@
+const shoppingList = [
+    milk = {
+        id: 1,
+        item: "milk",
+        price: 4,
+        organic: false
+    },
+    eggs = {
+        id: 2,
+        item: "eggs",
+        price: 2,
+        organic: true
+    },
+    pizzaRolls = {
+        id: 3,
+        item: "pizza rolls",
+        price: 5,
+        organic: false
+    },
+    steak = {
+        id: 4,
+        item: "steak",
+        price: 15,
+        organic: true
+    },
+    pineapple = {
+        id: 5,
+        item: "pineapple",
+        price: 3,
+        organic: true
+    },
+    
+
+]
+
+// Milk
+// Eggs
+// Pizza rolls
+// A protein
+// Your favorite fruit
+// Sandwich buns
+// Condiments
